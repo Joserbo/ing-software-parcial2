8298e535c76eac79d1ea9fc70fba4d5471cdf128
jose <jose@jose-HP-Pavilion-Notebook>
2019-04-24 19:11:23 -0600

    .

--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
@@ -5,6 +5,10 @@ import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service.ProductoServic
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +22,18 @@ public class ProductoController {
     @GetMapping("/buscarTodos")
     public List<ProductoEntity> buscarTodos() {
         return this.productoService.buscarTodos();
+    }
+    
+    @PostMapping("/registrarProducto")
+    public ProductoEntity registrarProducto(@RequestBody (required = true) ProductoEntity p1)
+    {
+        return this.productoService.registrarProducto(p1);
+    }
+    
+    @PutMapping("/actualizarStock/{id}/{operacion}/{cantidad}")
+    public String actualizarStock(@PathVariable(required = true) Long id, @PathVariable(required = true) String operacion, @PathVariable(required = true) Integer cantidad)
+    {
+        return this.productoService.actualizarStock(id, cantidad, operacion);
     }
     
 }
--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
@@ -2,6 +2,9 @@ package gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service;
 
 import gt.edu.umg.ingenieria.sistemas.core.parcial2.core.model.ProductoEntity;
 import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.dao.ProductoRepository;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,40 @@ public class ProductoService {
     private ProductoRepository productoRepository;
     
     public List<ProductoEntity> buscarTodos() {
-        return (List<ProductoEntity>) this.productoRepository.findAll();
+        List<ProductoEntity> lista = (List<ProductoEntity>) this.productoRepository.findAll();
+        Collections.sort(lista, Comparator.comparing(ProductoEntity::getName));
+        return lista;
+    }
+    
+    public ProductoEntity registrarProducto(ProductoEntity p1){
+        String name = p1.getName();
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        p1.setName(name);
+        return this.productoRepository.save(p1);        
+    }
+    
+    public String actualizarStock(Long id, Integer cantidad, String opcion){
+        ProductoEntity p1 = this.productoRepository.findById(id).get();
+        String mensaje = "operacion invalida";
+        if(opcion.equals("incrementar"))
+        {
+            Integer stock = p1.getStock();
+            stock = stock + cantidad;
+            p1.setStock(stock);
+            this.productoRepository.save(p1);
+            mensaje = "Stock actualizado [ "+p1.getName()+" "+stock+"]";
+            return mensaje;            
+        }
+        if(opcion.equals("decrementar"))
+        {
+            Integer stock = p1.getStock();
+            stock = stock - cantidad;
+            p1.setStock(stock);
+            this.productoRepository.save(p1);
+            mensaje = "Stock actualizado [ "+p1.getName()+" "+stock+"]";
+            return mensaje;            
+        }
+        else return mensaje;
     }
     
 }
